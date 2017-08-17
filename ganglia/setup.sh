ede1d17ebe3f67bbd11ee853e05affc2bd19fc10
Myungjun Son <mjs8612@gmail.com>
2017-08-18 03:39:40 +0900

    apache2 instead of httpd

--- ganglia/setup.sh
+++ ganglia/setup.sh
@@ -3,18 +3,18 @@
 /root/spark-ec2/copy-dir /etc/ganglia/
 
 # Start gmond everywhere
-/etc/init.d/gmond restart
+/etc/init.d/ganglia-monitor restart
 
 for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t -t $SSH_OPTS root@$node "/etc/init.d/gmond restart"
+  ssh -t -t $SSH_OPTS root@$node "/etc/init.d/ganglia-monitor restart"
 done
 
 # gmeta needs rrds to be owned by nobody
 chown -R nobody /var/lib/ganglia/rrds
 # cluster-wide aggregates only show up with this. TODO: Fix this cleanly ?
-ln -s /usr/share/ganglia/conf/default.json /var/lib/ganglia/conf/
+ln -s /usr/share/ganglia-webfrontend/conf.php /var/lib/ganglia-web/conf
 
 /etc/init.d/gmetad restart
 
 # Start http server to serve ganglia
-/etc/init.d/httpd restart
+/etc/init.d/apache2 restart
