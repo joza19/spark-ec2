d1dd2e59b5a984d49e526ddf4a2fc7f3739c1812
Myungjun Son <mjs8612@gmail.com>
2017-08-17 22:17:06 +0900

    changed the code for Ubuntu

--- rstudio/init.sh
+++ rstudio/init.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-# download rstudio 
-wget http://download2.rstudio.org/rstudio-server-rhel-0.99.446-x86_64.rpm
-sudo yum install --nogpgcheck -y rstudio-server-rhel-0.99.446-x86_64.rpm
+## download rstudio
+#wget http://download2.rstudio.org/rstudio-server-rhel-0.99.446-x86_64.rpm
+#sudo yum install --nogpgcheck -y rstudio-server-rhel-0.99.446-x86_64.rpm
+#
+## restart rstudio
+#rstudio-server restart
+#
+## add user for rstudio, user needs to supply password later on
+#adduser rstudio
+#
+## create a Rscript that connects to Spark, to help starting user
+#cp /root/spark-ec2/rstudio/startSpark.R /home/rstudio
 
-# restart rstudio 
-rstudio-server restart 
-
-# add user for rstudio, user needs to supply password later on
-adduser rstudio
-
-# create a Rscript that connects to Spark, to help starting user
-cp /root/spark-ec2/rstudio/startSpark.R /home/rstudio
-
+echo "Skipping rstudio installation on Ubuntu"
 # make sure that the temp dirs exist and can be written to by any user
 # otherwise this will create a conflict for the rstudio user
 function create_temp_dirs {
